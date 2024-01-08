7dc92f8db68650f7728235a7b22875f9a8c6a0b0
Duc Anh Nguyen <53148787+DepTrai95@users.noreply.github.com>
2024-01-08 13:39:04 +0100

    [FEATURE] set up scss and base setup in general for future projects

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,68 @@
+<template>
+  <HelloWorld msg="Hallo ich bin das Template für alle Websites" />
+</template>
+
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+<style lang="scss">
+@import './global.scss';
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+* {
+  box-sizing: border-box;
+  scroll-behavior: smooth;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+html {
+  background-color: $background-color;
+  color: $primary-color;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  font-size: 10px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+//delete then
+body {
+  margin: 0;
+  display: flex;
+  place-items: center;
+  min-width: 320px;
+  min-height: 100vh;
+}
+
+body {
+  @include responsive-font-size(1.8rem, 2.2rem);
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  overflow: hidden;
+}
+
+.inner {
+  margin: 0 auto;
+  max-width: $content-max-width - (2 * $content-padding);
+  padding: 0 20px;
+
+  @include for-tablet-portrait-up {
+    padding: 0 40px;
+  }
+
+  @include for-tablet-landscape-up {
+    padding: 0 140px;
+  }
+
+  @include for-desktop-up {
+    padding: 0 200px;
+  }
 }
 </style>
